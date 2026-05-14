53eb904180b09be52aac8a3b4591a53a51ff3594
parvejtalukder <mail@parvejhusentalukder.com>
2026-05-14 20:01:58 +0600

    Fresh restart - Certify project rebuilt

---
+++ src/main/java/com/pht/certify/CertifyApplication.java
@@ -0,0 +1,13 @@
+package com.pht.certify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertifyApplication.class, args);
+	}
+
+}
