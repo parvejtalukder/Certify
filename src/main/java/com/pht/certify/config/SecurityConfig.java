610cabc5439248f88b7071b5299d0f0612a2a2ce
parvejtalukder <mail@parvejhusentalukder.com>
2026-05-15 21:49:27 +0600

    Update: New changes on Spring Security

--- src/main/java/com/pht/certify/config/SecurityConfig.java
+++ src/main/java/com/pht/certify/config/SecurityConfig.java
@@ -1,5 +1,68 @@
 package com.pht.certify.config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
+import com.pht.certify.model.UserServices;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private final UserServices userServices;
+
+    public SecurityConfig(UserServices userServices) {
+        this.userServices = userServices;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsServices() {
+        return userServices; 
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userServices);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider; 
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/login","/add-user", "/add-certificate", "/certificate/**" , "/css/**", "/js/**", "/images/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .formLogin(form -> form
+                .loginPage("/login")
+                .permitAll()
+            )
+            .logout(logout -> logout
+                .permitAll()
+            );
+
+        return http.build();
+    }
 }
--- src/main/java/com/pht/certify/repository/UserRepo.java
+++ src/main/java/com/pht/certify/repository/UserRepo.java
@@ -1,7 +1,8 @@
 package com.pht.certify.repository;
+import java.util.Optional;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import com.pht.certify.model.Admin;
 
 public interface UserRepo extends MongoRepository<Admin, String> {
-
+   Optional<Admin> findByUsername(String username);
 }
